ea65401287d0f2be882fd02efdb105ab29f6aa55
Lenjons <lenjons@kth.se>
2014-02-05 16:58:40 +0100

    Med lol

---
+++ workspace/MainActivity/src/se/kth/csc/iprog/dinnerplanner/android/view/ExampleView.java
@@ -0,0 +1,39 @@
+package se.kth.csc.iprog.dinnerplanner.android.view;
+
+import java.util.Set;
+
+import se.kth.csc.iprog.dinnerplanner.android.R;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
+import android.view.View;
+import android.widget.TextView;
+
+public class ExampleView {
+
+	View view;
+
+	private DinnerModel model = new DinnerModel();
+	
+	public ExampleView(View view) {
+		String lolette = "";
+		model.setNumberOfGuests(4);
+		int banan = model.getNumberOfGuests();
+		String lol = Integer.toString(banan);
+		Set<Ingredient> apple = model.getAllIngredients();
+		for(Ingredient ing:apple){lolette = lolette +"\n"+ ing.getName();};
+		float price = model.getTotalMenuPrice();
+		String plice = Float.toString(price);
+		// store in the class the reference to the Android View
+		this.view = view;
+		
+		TextView example = (TextView) view.findViewById(R.id.example_text);
+		example.setText(lol);
+		
+		TextView example2 = (TextView) view.findViewById(R.id.lole);
+		example2.setText(plice);
+
+
+		// Setup the rest of the view layout
+	}
+
+}
